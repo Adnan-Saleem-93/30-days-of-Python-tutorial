3aaabefa07efacfc10ea9e9e5f03ac48c3f62ca6
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2025-08-28 01:02:33 +0500

    Add variables.py for Day 2 exercises, including variable declarations, type checks, and basic arithmetic operations

---
+++ day-2/variables.py
@@ -0,0 +1,106 @@
+# Day 2: 30 Days of python programming
+
+# EXERCISE 1
+
+# Declare a first name variable and assign a value to it
+from itertools import count
+from math import floor, remainder
+
+
+first_name = 'Adnan'
+print(first_name)
+# Declare a last name variable and assign a value to it
+last_name = 'Saleem'
+print(last_name)
+# Declare a full name variable and assign a value to it
+full_name = first_name + " " + last_name
+print(full_name)
+# Declare a country variable and assign a value to it
+country = 'Pakistan'
+print(country)
+# Declare a city variable and assign a value to it
+city = 'Rawalpindi'
+print(city)
+# Declare an age variable and assign a value to it
+age = 32
+print(age)
+# Declare a year variable and assign a value to it
+year = 1993
+print(year)
+# Declare a variable is_married and assign a value to it
+is_married = True
+print(is_married)
+# Declare a variable is_true and assign a value to it
+is_true = True
+print(is_true)
+# Declare a variable is_light_on and assign a value to it
+is_light_on = True
+print(is_light_on)
+# Declare multiple variables on one line
+has_car, favorite_numbers, star_sign = True, ['3','7'], "Leo"
+print(has_car, favorite_numbers, star_sign )
+
+# EXERCISE 2:
+
+# Check the data type of all your variables using type() built-in function
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(has_car))
+print(type(favorite_numbers))
+print(type(star_sign))
+# Using the len() built-in function, find the length of your first name
+print(len(first_name))
+# Compare the length of your first name and your last name
+print(len(first_name) - len(last_name))
+# Declare 5 as num_one and 4 as num_two
+num_one,num_two = 5,4
+print(num_one, num_two)
+# Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
+print(total)
+# Subtract num_two from num_one and assign the value to a variable diff
+diff = num_one - num_two
+print(diff)
+# Multiply num_two and num_one and assign the value to a variable product
+product = num_one * num_two
+print(product)
+# Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
+print(division)
+# Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two % num_one
+print(remainder)
+# Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_one ** num_two
+print(exp)
+# Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = floor(num_one / num_two)
+print(floor_division)
+# The radius of a circle is 30 meters.
+# Calculate the area of a circle and assign the value to a variable name of area_of_circle
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+print(area_of_circle)
+# Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circum_of_circle = 2 * 3.14 * radius
+print(circum_of_circle)
+# Take radius as user input and calculate the area.
+radius = input("Enter Radius: ")
+area_of_circle = 3.14 * int(radius) ** 2
+print(area_of_circle)
+# Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+first_name = input('Enter first name: ')
+last_name = input('Enter last name: ')
+country = input('Enter country: ')
+age = input('Enter age: ')
+print("first_name = ",first_name)
+print("last_name = ",last_name)
+print("country = ",country)
+print("age = ",age)
